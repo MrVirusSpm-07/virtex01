a8c15c0755787769e965fb9b78d49d551adf33b3
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-04-06 04:35:54 +0700

    Add files via upload

---
+++ virtexmematikan.sh
@@ -0,0 +1,488 @@
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+#RECORD_BY_MR_VIRUS_SPM
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+sleep 2
+echo "$blue__     _____ ____ _____ _______  __      ___  _  "
+echo "\ \   / /_ _|  _ \_   _| ____\ \/ /     / _ \/ | "
+echo " \ \ / / | || |_) || | |  _|  \  /_____| | | | | "
+
+echo "  \ V /  | ||  _ < | | | |___ /  \_____| |_| | | "
+echo "   \_/  |___|_| \_\|_| |_____/_/\_\     \___/|_| "
+
+echo "####################################################"
+echo
+sleep 1
+
+
+echo "$green( BY MR•VIRUS-SPM)"
+
+echo "(VIRTEX MEMATIKAN)"
+echo "*************************************************************"
+echo $white
+
+sleep 2
+
+echo "loading geng....................."
+
+
+
+
+sleep 2
+echo "masih loading...................."
+sleep 2
+
+echo "bentar lagi......................"
+sleep 1
+
+
+
+echo $yellow
+read -p "MASUKAN PASSWORD : " lol
+
+
+if [ $lol = 071003 ]
+then
+sleep 1
+echo $yellow
+echo "::::::::::::::::::::::::::::::::::::::::::"
+echo "$res(+) Author   : dicky andreano pangestu (+)"
+echo "(+) Cnl Yt   : MR VIRUS SPM NEW        (+)"
+
+
+echo "(+) NM IG    : mvs.x_x                 (+)"
+
+echo "$yellow::::::::::::::::::::::::::::::::::::::::::"
+echo $white
+
+sleep 2
+echo "ݝMRV1RU55PM🔥BY•MRVIRUSSPMݝ                                        "
+echo "ݝMRV1RU55PM🔥BY•MRVIRUSSPMݝ                                          "                                                                                      
+echo "💣SAYA MENCIPTAKAN VIRTEX INI BUAT ATACK GRUP PENGHINA AGAMA BUKAN UNTUK NGERJAIN TEMEN ATAU PANSOS💣"
+echo "https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ"
+echo "*999999999999* (KEMATIAN) *9999999999999*"
+echo
+echo "*999999999999* (KEMATIAN) *9999999999999*"
+echo
+echo "*999999999999* (KEMATIAN) *9999999999999*"
+echo
+echo "*999999999999* (KEMATIAN) *9999999999999*"
+echo
+echo "*999999999999* (KEMATIAN) *9999999999999*"
+echo
+echo "*999999999999* (KEMATIAN) *9999999999999*"
+echo
+echo "*999999999999* (KEMATIAN) *9999999999999*"
+echo "*https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "*9999999* [bocil virtex kematian] *9999999*"
+echo "AWAS NGAB TAKUT HP ELU MELEDAK KALO GK KUAT"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo "*88888* _BY•MR•VIRUS-SPM_ *88888*"
+echo "*88888*  🔥🔥🔥🔥🔥🔥  *88888*"
+echo
+echo
+fi
